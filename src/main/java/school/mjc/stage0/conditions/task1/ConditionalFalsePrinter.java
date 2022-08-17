c3ae2f5d4b64b9678e9284552872b8b2d8433ad1
madina <91madina@gmail.com>
2022-08-17 21:00:04 +0600

    Module #4, Task #1

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        if (false){
+            System.out.println("Should not go there");
+        } else {
+            System.out.println("false");
+        }
     }
 }
